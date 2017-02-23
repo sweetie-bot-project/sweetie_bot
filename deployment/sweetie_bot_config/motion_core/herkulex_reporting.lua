15edf72d10b1fc952e3c7070e97c49139c01818a
Oleg Goncharov <goncharovoi@yandex.ru>
2017-02-23 18:02:33 +0300

    reporting subsystem.

--- deployment/sweetie_bot_config/motion_core/herkulex_reporting.lua
+++ deployment/sweetie_bot_config/motion_core/herkulex_reporting.lua
@@ -1,16 +1,47 @@
-# LOAD REPORTING COMPONET FOR REPORTING HERKULEX_SCHED
+-- 
+-- HERKULEX_* SUBSYSTEM REPORTING
+--
+-- Setup reporter components for servo_inv and herkulex_sched.
+-- Component 
+--
+-- Intended to be run via config script.
+--
 
-# statistics
-loadComponent("reporter_herk_stat", "OCL::FileReporting");
-reporter_herk_stat.WriteHeader = 0;
-reporter_herk_stat.ReportFile = "statistics.out";
-addPeer("reporter_herk_stat", "herk_sched");
-reporter_herk_stat.reportPort("herk_sched", "statistics");
+reporting = {}
 
-# servo detailed states
-loadComponent("reporter_herk_state", "OCL::FileReporting");
-reporter_herk_state.WriteHeader = 0;
-reporter_herk_state.ComplexDecompose = 1;
-reporter_herk_state.ReportFile = "state.out";
-addPeer("reporter_herk_state", "herk_sched");
-reporter_herk_state.reportPort("herk_sched", "out_states");
+-- get directory
+local directory = rttlib_extra.ros.get_param_string("~reporting/directory")
+directory = directory or "~"
+
+-- helper function to setup reporter
+local function setup_reporting(reporter_name, peer_ports, file, complex_decompose)
+	complex_decompose = complex_decompose or false
+	-- load reporter 
+	if not depl:loadComponent("reporting/" .. reporter_name, "OCL::FileReporting") then
+		return false
+	end
+	reporter = depl:getPeer("reporting/" .. reporter_name)
+	reporting[reporter_name] = reporter
+	-- configuere reporter: default
+	reporter:getProperty("WriteHeader"):set(true)
+	reporter:getProperty("ReportFile"):set(file)
+	-- configuere reporter: rosparam 
+	rttlib_extra.ros.get_peer_params(reporter)
+	-- TODO rosparam
+	-- TODO period
+	-- add access to peers
+	for i, peer in ipairs(peer_ports) do
+		depl:addPeer("reporting/" .. reporter_name, peer.name)
+		-- report ports
+		for i, port in ipairs(peer.ports) do
+			reporter:reportPort(peer.name, port)
+		end
+	end
+end
+
+-- herkulex_sched statistics
+setup_reporting("herkulex_statistics", { { name = "herkulex_sched" ,  ports= {"statistics"} } }, directory .. "/statistics.out")
+-- servo states
+setup_reporting("herkulex_states", { { name = "herkulex_sched" ,  ports= {"out_states"} } }, directory .. "/states.out")
+-- control signals
+setup_reporting("herkulex_control", { { name = "agregator_ref" ,  ports= {"out_joints_sorted"} }, { name = "agregator_real" ,  ports= {"out_joints_sorted"} } }, directory .. "/control.out")
