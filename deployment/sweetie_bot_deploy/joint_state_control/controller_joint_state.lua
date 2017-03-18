9fec1d2c0b3bacaacb59bf86819596c8215d3518
disrecord <goncharovoi@yandex.ru>
2017-03-18 22:48:17 +0300

    Trajectory filter for FollowJointState controller.

--- deployment/sweetie_bot_deploy/joint_state_control/controller_joint_state.lua
+++ deployment/sweetie_bot_deploy/joint_state_control/controller_joint_state.lua
@@ -1,5 +1,5 @@
 --
--- VIRTUAL ROBOT and JOINT STATE controllers
+-- JOINT STATE controllers
 --
 -- Setup FollowJointState controller
 --
@@ -13,18 +13,24 @@ controller = controller or {}
 ros:import("sweetie_bot_controller_joint_space")
 depl:loadComponent("controller/joint_state", "sweetie_bot::motion::controller::FollowJointState")
 controller.joint_state = depl:getPeer("controller/joint_state")
-rttlib_extra.get_peer_rosparams(controller.joint_state)
 -- register controller
 resource_control.register_controller(controller.joint_state)
 -- timer
 depl:connect(timer.controller.port, "controller/joint_state.sync", rtt.Variable("ConnPolicy"))
 -- data flow: controller <-> agregator_ref
 depl:connect("controller/joint_state.out_joints_ref_fixed", "agregator_ref.in_joints", rtt.Variable("ConnPolicy"))
-depl:connect("agregator_ref.out_joints_sorted", "controller/joint_state.in_joints_sorted", rtt.Variable("ConnPolicy"))
--- ros redirect
+depl:connect("controller/joint_state.in_joints_sorted", "agregator_ref.out_joints_sorted", rtt.Variable("ConnPolicy"))
+-- ROS redirect
 depl:stream("controller/joint_state.in_joints_ref", ros:topic("~controller/joint_state/in_joints_ref"))
 depl:stream("controller/joint_state.out_joints_src_reset", ros:topic("~controller/joint_state/out_joints_src_reset"))
 -- connect to RobotModel
 depl:connectServices("controller/joint_state", "agregator_ref")
+-- load trajectory filter
+controller.joint_state:loadService("transient_joint_state_exp");
+-- get ROS parameteres
+rttlib_extra.get_peer_rosparams(controller.joint_state)
+-- advertise ROS operation
+controller.joint_state:loadService("rosservice")
+controller.joint_state:provides("rosservice"):connect("rosSetOperational", config.node_fullname .. "controller/joint_state/set_operational", "std_srvs/SetBool")
 -- prepare to start
 controller.joint_state:configure()
