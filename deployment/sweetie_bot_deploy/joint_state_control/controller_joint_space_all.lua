c3da79b6b799d8fca8fa952e69af2ceab39c8eea
disrecord <goncharovoi@yandex.ru>
2017-03-18 22:52:39 +0300

    TorqueMainSwitch is run by default is real robot enviroment.

--- deployment/sweetie_bot_deploy/joint_state_control/controller_joint_space_all.lua
+++ deployment/sweetie_bot_deploy/joint_state_control/controller_joint_space_all.lua
@@ -1,11 +1,22 @@
 --
 -- VIRTUAL ROBOT and JOINT STATE controllers
 --
--- controllers: FollowJointState, AnimJointTrajectoryBase
---
+-- controllers: FollowJointState, AnimJointTrajectoryBase, TorqueMainSwitch if herkulex and agregator are present.
+--.
 -- Intended to be run via config script.
 --
 require "controller_joint_state"
 assert(controller.joint_state:start())
 
 require "controller_joint_trajectory"
+
+if (agregator_real and herkulex) then
+	require "controller_torque_off"
+end
+
+-- require "reporting"
+-- 
+-- dir = "/home/oleg/"
+-- reporting.add_filereporter("joint_state", { { name = "controller/joint_state" ,  ports= {"out_joints_ref_fixed"} } }, dir .. "joint_state.out", true)
+-- reporting.add_filereporter("joint_trajectory", { { name = "controller/joint_trajectory" ,  ports= {"out_joints_ref_fixed"} } }, dir .. "joint_traject.out", true)
+-- reporting.add_filereporter("agregator", {  { name = "agregator_ref" ,  ports= {"out_joints_sorted"} } }, dir .. "agregator.out", true)
