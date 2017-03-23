333d9e369f5dff33e760cc3997c580c696b1dddc
disrecord <goncharovoi@yandex.ru>
2017-03-23 19:16:22 +0300

    Switch voice node to TextCommand control.

--- highlevel/sweetie_bot_voice/src/voice/main.py
+++ highlevel/sweetie_bot_voice/src/voice/main.py
@@ -2,40 +2,47 @@
 # -*- coding: utf-8 -*-
 
 import rospy, roslib, rospkg, os, sys, random
-from sound_play.msg import SoundRequest
-from std_msgs.msg import String
-
 from sound_play.libsoundplay import SoundClient
 
+from std_msgs.msg import String
+from sweetie_bot_text_msgs.msg import TextCommand
+
 soundhandle = SoundClient()
-path = '';
-sounds = [];
+sounds = {}
 
-def sleep(t):
-    try:
-        rospy.sleep(t)
-    except:
-        pass
+def commandCallback(cmd):
+    global soundhandle, sounds;
+    if cmd.type == 'voice/play_wav':
+        if cmd.command in sounds:
+            filename = sounds[cmd.command]
+            rospy.loginfo('Playing sound: {0} ({1}).'.format(cmd.command, filename))
+            snd = soundhandle.waveSound(filename)
+            snd.play()
+        else:
+            rospy.logerror('Unknown play_wav sound: ' + cmd.command)
 
-def chatterCallback(data):
-    global soundhandle, path, sounds;
-    rospy.loginfo(rospy.get_caller_id() + " [%s]", data.data)
-    rospy.loginfo(path)
-    snd = soundhandle.waveSound(path + data.data  + '.wav')
-    snd.play()
-    #rate.sleep()
+
+def file_dict(directory, ext):
+    files = {} 
+    for f in os.listdir(directory):
+        basename, extension = os.path.splitext(f)
+        print basename, ' ', extension
+        if extension.endswith(ext):
+            files[basename] = os.path.join(directory, f)
+    return files
+
 
 def main():
-    global soundhandle, path, sounds;
+    global soundhandle, sounds;
     rospy.init_node('voice', anonymous = True)
-    rospy.Subscriber("voice", String, chatterCallback)
+    rospy.Subscriber("voice", TextCommand, commandCallback)
 
     rospack = rospkg.RosPack()
     path = rospack.get_path('sweetie_bot_voice') + '/sounds/'
-    rospy.loginfo(path)
+    rospy.loginfo("Sound files path: " + path)
 
-    sounds = rospy.get_param('~sounds', '[]')
-    rospy.loginfo(sounds)
+    sounds = file_dict(path, 'wav')
+    rospy.loginfo('Found sounds:\n' + repr(sounds))
 
     rospy.sleep(1)
     
