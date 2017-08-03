Fix hoof pose calculation. Varios erors are fixed.

behaviors/behavior_brohoof/src/behavior_brohoof/brohoof_sm.py
@@ -14,9 +14,14 @@ from flexbe_manipulation_states.srdf_state_to_moveit import SrdfStateToMoveit
 from flexbe_states.wait_state import WaitState
 from sweetie_bot_flexbe_states.text_command_state import TextCommandState
 from flexbe_manipulation_states.moveit_to_joints_state import MoveitToJointsState
+from flexbe_manipulation_states.get_joint_values_state import GetJointValuesState
+from flexbe_states.decision_state import DecisionState
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
+import math
+import tf
 from tf.transformations import quaternion_about_axis
+from flexbe_core.proxy import ProxyTransformListener
 
 from std_msgs.msg import Header
 from geometry_msgs.msg import Point, Quaternion, Pose, PoseStamped
@@ -42,11 +47,14 @@ Robot is assumed to be standing on four legs.
 		# parameters of this behavior
 		self.add_parameter('wait_time', 3)
 		self.add_parameter('neck_angle', 0.25)
+		self.add_parameter('hoof_shift', 0)
 
 		# references to used behaviors
 
 		# Additional initialization code can be added inside the following tags
 		# [MANUAL_INIT]
+
+                self._tf = ProxyTransformListener().listener()
 		
 		# [/MANUAL_INIT]
 
@@ -67,23 +75,23 @@ Robot is assumed to be standing on four legs.
 
 
 		with _state_machine:
-			# x:109 y:58
-			OperatableStateMachine.add('CalculateTargetPose',
-										CalculationState(calculation=self.calculateTargetPoseFunc),
-										transitions={'done': 'CheckReacibility'},
-										autonomy={'done': Autonomy.Off},
-										remapping={'input_value': 'pose', 'output_value': 'pose'})
+			# x:109 y:195
+			OperatableStateMachine.add('CheckPoseHeader',
+										DecisionState(outcomes=['good','bad'], conditions=lambda x: 'good' if x.header.frame_id else 'bad'),
+										transitions={'good': 'CalculateTargetPose', 'bad': 'unreachable'},
+										autonomy={'good': Autonomy.Off, 'bad': Autonomy.Off},
+										remapping={'input_value': 'pose'})
 
-			# x:347 y:54
+			# x:286 y:54
 			OperatableStateMachine.add('CheckReacibility',
 										MoveitToPose(move_group='leg1', plan_only=True, position_tolerance=0.001, orientation_tolerance=0.001),
-										transitions={'reached': 'RaiseHead', 'planning_failed': 'unreachable', 'control_failed': 'failed'},
+										transitions={'reached': 'GetHeadPose', 'planning_failed': 'unreachable', 'control_failed': 'failed'},
 										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off},
 										remapping={'pose': 'pose'})
 
 			# x:870 y:62
 			OperatableStateMachine.add('RaiseLeg',
-										SrdfStateToMoveit(config_name='leg1_low_raised_shift', move_group='legs_front', action_topic='move_group', robot_name=''),
+										SrdfStateToMoveit(config_name='low_raised1_shift', move_group='legs_front', action_topic='move_group', robot_name=''),
 										transitions={'reached': 'MoveLeg', 'planning_failed': 'unreachable', 'control_failed': 'failed', 'param_error': 'failed'},
 										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
 										remapping={'config_name': 'config_name', 'move_group': 'move_group', 'robot_name': 'robot_name', 'action_topic': 'action_topic', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
@@ -128,12 +136,33 @@ Robot is assumed to be standing on four legs.
 										transitions={'done': 'Wait'},
 										autonomy={'done': Autonomy.Off})
 
-			# x:615 y:51
+			# x:648 y:42
 			OperatableStateMachine.add('RaiseHead',
-										MoveitToJointsState(move_group='head', joint_names=['joint53'], action_topic='move_group'),
+										MoveitToJointsState(move_group='head', joint_names=['joint51', 'joint52', 'joint53', 'joint54'], action_topic='move_group'),
 										transitions={'reached': 'RaiseLeg', 'planning_failed': 'failed', 'control_failed': 'failed'},
 										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off},
-										remapping={'joint_config': 'joint53_pose'})
+										remapping={'joint_config': 'head_pose'})
+
+			# x:461 y:3
+			OperatableStateMachine.add('GetHeadPose',
+										GetJointValuesState(joints=['joint51', 'joint52', 'joint53', 'joint54']),
+										transitions={'retrieved': 'CalcRaisedHeadPose'},
+										autonomy={'retrieved': Autonomy.Off},
+										remapping={'joint_values': 'head_pose'})
+
+			# x:459 y:94
+			OperatableStateMachine.add('CalcRaisedHeadPose',
+										CalculationState(calculation=self.calculateHeadPoseFunc),
+										transitions={'done': 'RaiseHead'},
+										autonomy={'done': Autonomy.Off},
+										remapping={'input_value': 'head_pose', 'output_value': 'head_pose'})
+
+			# x:109 y:58
+			OperatableStateMachine.add('CalculateTargetPose',
+										CalculationState(calculation=self.calculateTargetPoseFunc),
+										transitions={'done': 'CheckReacibility'},
+										autonomy={'done': Autonomy.Off},
+										remapping={'input_value': 'pose', 'output_value': 'pose'})
 
 
 		return _state_machine
@@ -141,16 +170,27 @@ Robot is assumed to be standing on four legs.
 
 	# Private functions can be added inside the following tags
 	# [MANUAL_FUNC]
+        def calculateHeadPoseFunc(self, joints):
+            joints = list(joints)
+            joints[0] = 0.0
+            joints[2] = self.neck_angle
+            return joints
 
-        def calculateTargetPoseFunc(input_pose):
+        def calculateTargetPoseFunc(self, input_pose):
             '''
             Calculate target pose for leg1. 
             '''
-            output_pose = PoseStamped()
-            output_pose.header = Header(frame_id = input_pose.header.frame_id)
-            # TODO move point forward along Oy without dependence on orientation of `leap_motion` frame
+            try:
+                output_pose = self._tf.transformPose('base_link', PoseStamped(Header(frame_id = input_pose.header.frame_id), input_pose.pose))
+            except tf.Exception as e:
+                Logger.logerr('Cannot transform from `%s` to `base_link` frame.' % input_pose.header.frame_id)
+                return None
+
+            # move point forward along Oy of base_link
             output_pose.pose.position = Point(input_pose.pose.position.x, input_pose.pose.position.y + self.hoof_shift, input_pose.pose.position.z)
-            output_pose.pose.orientation = quaternion_about_axis([0, 0, 1], math.pi/2) * input_pose.pose.orientation
+            # output_pose.pose.orientation = quaternion_about_axis([1, 0, 0], math.pi/2) * input_pose.pose.orientation
+            output_pose.pose.orientation = Quaternion(0, 0, 0, 1)
+            Logger.loginfo('calculateTargetPoseFunc: target hoof pose: %s' % str(output_pose))
             return output_pose
 
 	