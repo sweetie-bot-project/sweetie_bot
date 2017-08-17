70a25a1ffd205b6a2ee9c4961aa2abef9956aecf
disrecord <goncharovoi@yandex.ru>
2017-08-17 12:52:50 +0300

    Add lang prefix support.

--- highlevel/sweetie_bot_voice/src/voice/main.py
+++ highlevel/sweetie_bot_voice/src/voice/main.py
@@ -37,8 +37,10 @@ def main():
     rospy.init_node('voice', anonymous = True)
     rospy.Subscriber("voice", TextCommand, commandCallback)
 
+    lang_prefix = rospy.get_param('~lang', 'ru')
+
     rospack = rospkg.RosPack()
-    path = rospack.get_path('sweetie_bot_voice') + '/sounds/'
+    path = os.path.join(rospack.get_path('sweetie_bot_voice'), 'sounds' , lang_prefix)
     rospy.loginfo("Sound files path: " + path)
 
     sounds = file_dict(path, 'wav')
