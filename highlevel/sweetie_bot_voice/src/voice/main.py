b328368e58c527b89eff0817f8116ed37c7b3e0b
disrecord <goncharovoi@yandex.ru>
2017-11-03 01:13:30 +0300

    Add mixed language mode.
    Lang `ru,en` means that robot can use russian and english prases. But
    russian prases are preferred.

--- highlevel/sweetie_bot_voice/src/voice/main.py
+++ highlevel/sweetie_bot_voice/src/voice/main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import string
 import rospy, roslib, rospkg, os, sys, random
 from sound_play.libsoundplay import SoundClient
 
-from std_msgs.msg import String
 from sweetie_bot_text_msgs.msg import TextCommand
 
 soundhandle = SoundClient()
@@ -27,7 +27,6 @@ def file_dict(directory, ext):
     try:
         for f in os.listdir(directory):
             basename, extension = os.path.splitext(f)
-            print basename, ' ', extension
             if extension.endswith(ext):
                 files[basename] = os.path.join(directory, f)
     except OSError as e:
@@ -41,14 +40,17 @@ def main():
     rospy.init_node('voice', anonymous = True)
     rospy.Subscriber('control', TextCommand, commandCallback)
 
-    lang_prefix = rospy.get_param('lang', 'ru')
+    lang_prefixes = string.split(rospy.get_param('lang', 'ru,en'), ',')
 
     rospack = rospkg.RosPack()
-    path = os.path.join(rospack.get_path('sweetie_bot_voice'), 'sounds' , lang_prefix)
-    rospy.loginfo("Sound files path: " + path)
+    sounds_path = os.path.join(rospack.get_path('sweetie_bot_voice'), 'sounds')
+    paths = [ os.path.join(sounds_path, prefix) for prefix in lang_prefixes ]
 
-    sounds = file_dict(path, 'wav')
-    rospy.loginfo('Found sounds:\n' + repr(sounds))
+    sounds = {}
+    for p in reversed(paths):
+        sounds.update(file_dict(p, 'wav'))
+
+    rospy.loginfo('Registered sounds:\n' + repr(sounds))
 
     rospy.sleep(1)
     
