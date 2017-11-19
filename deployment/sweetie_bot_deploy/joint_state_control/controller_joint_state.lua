76e0a74a3c596b5c9149774f70fd8d568a6187e8
disrecord <goncharovoi@yandex.ru>
2017-11-19 12:17:34 +0300

    djust deployment procedure to filters name change and services auto-loading support.

--- deployment/sweetie_bot_deploy/joint_state_control/controller_joint_state.lua
+++ deployment/sweetie_bot_deploy/joint_state_control/controller_joint_state.lua
@@ -13,6 +13,8 @@ controller = controller or {}
 ros:import("sweetie_bot_controller_joint_space")
 depl:loadComponent("controller/joint_state", "sweetie_bot::motion::controller::FollowJointState")
 controller.joint_state = depl:getPeer("controller/joint_state")
+-- get ROS parameteres and services
+rttlib_extra.get_peer_rosparams(controller.joint_state)
 -- register controller
 resource_control.register_controller(controller.joint_state)
 -- timer
@@ -25,10 +27,6 @@ depl:stream("controller/joint_state.in_joints_ref", ros:topic("~controller/joint
 depl:stream("controller/joint_state.out_joints_src_reset", ros:topic("~controller/joint_state/out_joints_src_reset"))
 -- connect to RobotModel
 depl:connectServices("controller/joint_state", "agregator_ref")
--- load trajectory filter
-controller.joint_state:loadService("transient_joint_state_exp")
--- get ROS parameteres
-rttlib_extra.get_peer_rosparams(controller.joint_state)
 -- advertise ROS operation
 controller.joint_state:loadService("rosservice")
 controller.joint_state:provides("rosservice"):connect("rosSetOperational", config.node_fullname .. "/controller/joint_state/set_operational", "std_srvs/SetBool")
--- deployment/sweetie_bot_deploy/joint_state_control/controller_joint_trajectory.lua
+++ deployment/sweetie_bot_deploy/joint_state_control/controller_joint_trajectory.lua
@@ -16,9 +16,7 @@ ros:import("sweetie_bot_controller_joint_space")
 
 depl:loadComponent("controller/joint_trajectory", "sweetie_bot::motion::controller::AnimJointTrajectory")
 controller.joint_trajectory = depl:getPeer("controller/joint_trajectory")
--- load trajectory filter
--- controller.joint_trajectory:loadService("transient_joint_state_exp");
--- load parameters
+-- load parameters and services
 rttlib_extra.get_peer_rosparams(controller.joint_trajectory)
 -- register controller
 resource_control.register_controller(controller.joint_trajectory)
