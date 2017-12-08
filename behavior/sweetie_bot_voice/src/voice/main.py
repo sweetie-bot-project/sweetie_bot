d31720cfa91d0621961e0cdf60cd90683d573651
disRecord <goncharovoi@yandex.ru>
2017-12-08 16:38:52 +0300

    Add sound_path parameter.

--- behavior/sweetie_bot_voice/src/voice/main.py
+++ behavior/sweetie_bot_voice/src/voice/main.py
@@ -12,7 +12,9 @@ sounds = {}
 
 def commandCallback(cmd):
     global soundhandle, sounds;
+    # check command type
     if cmd.type == 'voice/play_wav':
+        # play specified sound file
         if cmd.command in sounds:
             filename = sounds[cmd.command]
             rospy.loginfo('Playing sound: {0} ({1}).'.format(cmd.command, filename))
@@ -23,6 +25,14 @@ def commandCallback(cmd):
 
 
 def file_dict(directory, ext):
+    ''' Return list of files with given extension.
+
+        Paramters:
+            directory (string) --- directory name,
+            ext (string) --- file extension,
+
+        Return: list of strings.
+    '''
     files = {} 
     try:
         for f in os.listdir(directory):
@@ -40,20 +50,23 @@ def main():
     rospy.init_node('voice', anonymous = True)
     rospy.Subscriber('control', TextCommand, commandCallback)
 
+    # Get language settings
     lang_prefixes = string.split(rospy.get_param('lang', 'ru,en'), ',')
-
-    rospack = rospkg.RosPack()
-    sounds_path = os.path.join(rospack.get_path('sweetie_bot_voice'), 'sounds')
+   
+    # Get sounds location
+    sound_path = rospy.get_param('sound_path', '')
+    if not sound_path: 
+        rospack = rospkg.RosPack()
+        sounds_path = os.path.join(rospack.get_path('sweetie_bot_voice'), 'sounds')
+   
+    # List sound files.
     paths = [ os.path.join(sounds_path, prefix) for prefix in lang_prefixes ]
-
     sounds = {}
     for p in reversed(paths):
         sounds.update(file_dict(p, 'wav'))
-
     rospy.loginfo('Registered sounds:\n' + repr(sounds))
 
     rospy.sleep(1)
-    
     soundhandle.stopAll()
 
     rospy.spin()
