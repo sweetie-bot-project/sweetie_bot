071dc27a9dd00b27435d892dfddb7fb31c1c30e9
mutronics <fsdfur@gmail.com>
2019-07-09 23:09:04 +0300

    TestStepSequence looped

--- sweetie_bot_flexbe_behaviors/src/sweetie_bot_flexbe_behaviors/teststepsequence_sm.py
+++ sweetie_bot_flexbe_behaviors/src/sweetie_bot_flexbe_behaviors/teststepsequence_sm.py
@@ -45,7 +45,7 @@ class TestStepSequenceSM(Behavior):
 
 
 	def create(self):
-		# x:769 y:460, x:163 y:383
+		# x:365 y:575, x:163 y:383
 		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
 		_state_machine.userdata.trajectory_param = None
 
@@ -68,18 +68,36 @@ class TestStepSequenceSM(Behavior):
 										transitions={'success': 'EndWalk', 'partial_movement': 'failed', 'invalid_pose': 'failed', 'failure': 'failed'},
 										autonomy={'success': Autonomy.Off, 'partial_movement': Autonomy.Off, 'invalid_pose': Autonomy.Off, 'failure': Autonomy.Off})
 
-			# x:528 y:274
+			# x:479 y:36
 			OperatableStateMachine.add('EndWalk',
 										SetCartesianPose(controller='motion/controller/stance', pose=[0.0,0.0,0.233,0.0,0.0,0.0], frame_id='base_link_path', frame_is_moving=False, resources=['leg1','leg2','leg3','leg4'], actuator_frame_id='base_link', tolerance_lin=0.001, tolerance_ang=0.0085, timeout=10.0),
 										transitions={'done': 'Greeting', 'failed': 'failed', 'timeout': 'failed'},
 										autonomy={'done': Autonomy.Off, 'failed': Autonomy.Off, 'timeout': Autonomy.Off})
 
-			# x:731 y:309
+			# x:670 y:144
 			OperatableStateMachine.add('Greeting',
 										ExecuteStoredJointTrajectoryState(action_topic='motion/controller/joint_trajectory', trajectory_param='/saved_msgs/joint_trajectory/greeting'),
-										transitions={'success': 'finished', 'partial_movement': 'failed', 'invalid_pose': 'failed', 'failure': 'failed'},
+										transitions={'success': 'PrepareToWalk2', 'partial_movement': 'failed', 'invalid_pose': 'failed', 'failure': 'failed'},
 										autonomy={'success': Autonomy.Off, 'partial_movement': Autonomy.Off, 'invalid_pose': Autonomy.Off, 'failure': Autonomy.Off},
 										remapping={'result': 'result'})
+
+			# x:932 y:428
+			OperatableStateMachine.add('Turn45',
+										ExecuteStepSequence(controller='motion/controller/step_sequence', trajectory_param='turn_left_45', trajectory_ns='saved_msgs/step_sequence'),
+										transitions={'success': 'EndWalk2', 'partial_movement': 'failed', 'invalid_pose': 'failed', 'failure': 'failed'},
+										autonomy={'success': Autonomy.Off, 'partial_movement': Autonomy.Off, 'invalid_pose': Autonomy.Off, 'failure': Autonomy.Off})
+
+			# x:792 y:546
+			OperatableStateMachine.add('EndWalk2',
+										SetCartesianPose(controller='motion/controller/stance', pose=[0.0,0.0,0.2085,0.0,0.0,0.0], frame_id='base_link_path', frame_is_moving=False, resources=['leg1','leg2','leg3','leg4'], actuator_frame_id='base_link', tolerance_lin=0.001, tolerance_ang=0.0085, timeout=10.0),
+										transitions={'done': 'WalkFwd', 'failed': 'failed', 'timeout': 'failed'},
+										autonomy={'done': Autonomy.Off, 'failed': Autonomy.Off, 'timeout': Autonomy.Off})
+
+			# x:741 y:286
+			OperatableStateMachine.add('PrepareToWalk2',
+										SetCartesianPose(controller='motion/controller/stance', pose=[0.0,0.0,0.2085,0.0,0.0,0.0], frame_id='base_link_path', frame_is_moving=False, resources=['leg1','leg2','leg3','leg4'], actuator_frame_id='base_link', tolerance_lin=0.001, tolerance_ang=0.0085, timeout=10.0),
+										transitions={'done': 'Turn45', 'failed': 'failed', 'timeout': 'failed'},
+										autonomy={'done': Autonomy.Off, 'failed': Autonomy.Off, 'timeout': Autonomy.Off})
 
 
 		return _state_machine
