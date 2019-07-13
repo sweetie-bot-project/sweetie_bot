08bf8191b2a8fd6ee11b38ead4e7d79e122acae2
disRecord <goncharovoi@yandex.ru>
2019-07-13 23:52:53 +0300

    ExecuteStepSequence state refactoring.
    1. Superclass ExecuteStepSequenceBase.
    2. Support for input keys in ExecuteStepSequenceKey.

--- sweetie_bot_flexbe_states/src/sweetie_bot_flexbe_states/execute_step_sequence_key.py
+++ sweetie_bot_flexbe_states/src/sweetie_bot_flexbe_states/execute_step_sequence_key.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
-from internal.execute_step_sequence_base import ExecuteStepSequenceBase as EventState 
+from flexbe_core import Logger
+
+from internal.execute_step_sequence_base import ExecuteStepSequenceBase as EventState
 
 # Superclass is imported as EventState to allow to parse state definition FlexApp correctly.
-class ExecuteStepSequence(EventState):
+class ExecuteStepSequenceKey(EventState):
     '''
     Execute stored sweetie_bot_control_msgs.msg.FollowStepSequence action. Goal is stored on ROS parameter server in serialized form.
     Parameter name is specified as state parameter.
 
     -- controller          string    Action server to execute action.
-    -- trajectory_param    string    ROS parameter in trajectory_ns which stores FollowStepSequence message.
     -- trajectory_ns       string    Namespace where trajectores are stored. 
+
+    ># trajectory_param    string    ROS parameter in trajectory_ns which stores FollowStepSequence message.
 
     <= success              Indicate that goal is achived.
     <= partial_movement     Execution stopped in midway (path or goal tolerance error, obstacle, external cancel request).
@@ -18,17 +21,23 @@ class ExecuteStepSequence(EventState):
 
     '''
 
-    def __init__(self, controller = 'motion/controller/step_sequence', trajectory_param = '', trajectory_ns = 'saved_msgs/step_sequence'):
+    def __init__(self, controller = 'motion/controller/step_sequence', trajectory_ns = 'saved_msgs/joint_trajectory'):
         # Declare outcomes and output keys
         # Actually constructor of ExecuteStepSequenceBase is called instead of constructor of EventState.
-        # 
+        super(ExecuteStepSequenceKey, self).__init__(controller = controller, outcomes = ['success', 'partial_movement', 'invalid_pose', 'failure'], input_keys = ['trajectory_param'])
 
-        super(ExecuteStepSequence, self).__init__(controller = controller, outcomes = ['success', 'partial_movement', 'invalid_pose', 'failure'])
-
-        # Load FollowStepSequenceGoal from Parameter Server
-        self._goal = self.load_goal_msg(trajectory_ns, trajectory_param)
+        self._trajectory_ns = trajectory_ns
 
     def on_enter(self, userdata):
+        self._error = False
+        # Load FollowStepSequenceGoal from Parameter Server
+        try:
+            goal = self.load_goal_msg(self._trajectory_ns, userdata.trajectory_param)
+        except Exception as e:
+            Logger.logwarn('ExecuteStepSequenceKey: unable to load trajectory from `%s/%s` parameter:\n%s' % (self._trajectory_ns, userdata.trajectory_param, str(e)) )
+            self._error = True
+            return
+
         # send loaded goal
-        self.send_goal(self._goal)
+        self.send_goal(goal)
 
---
+++ sweetie_bot_flexbe_states/src/sweetie_bot_flexbe_states/internal/execute_step_sequence_base.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+import copy
+
+from flexbe_core import EventState as Dummy
+from flexbe_core import Logger
+from flexbe_core.proxy import ProxyActionClient
+
+# modules
+import rospy
+import actionlib
+import xmlrpclib
+
+# datatypes
+from rospy.rostime import Duration
+from sweetie_bot_control_msgs.msg import FollowStepSequenceAction
+from sweetie_bot_control_msgs.msg import FollowStepSequenceGoal
+from sweetie_bot_control_msgs.msg import FollowStepSequenceResult
+from actionlib_msgs.msg import GoalStatus
+
+# This is helper class so trick FlexBe App to  ignore it. 
+# Dummy is actually EventState but FlexBe App is not able to recognize it.
+class ExecuteStepSequenceBase(Dummy): 
+    '''
+    Base class for control_msgs::FollowStepSequence action FlexBe states. It fully implements interaction wit action server
+    but does not implement FollowStepSequenceGoal message retriving logic.
+
+    -- controller          string    Action server to execute action.
+
+    <= success              Indicate that goal is achived.
+    <= partial_movement     Execution stopped in midway (path or goal tolerance error, obstacle, external cancel request).
+    <= invalid_pose         Initial pose is invalid, movement cannot be started.
+    <= failure              Any other failure. (Invalid joints, ActionServer unavailable and so on).
+
+    '''
+
+    def __init__(self, controller = 'motion/controller/step_sequence', outcomes = ['success', 'partial_movement', 'invalid_pose', 'failure'], input_keys = [], *args, **kwargs):
+        # Declare outcomes and output keys
+        super(ExecuteStepSequenceBase, self).__init__(outcomes = outcomes, input_keys = input_keys, *args, **kwargs)
+
+        # Connect to action server.
+        self._controller = controller
+        self._client = ProxyActionClient({self._controller: FollowStepSequenceAction}) # pass required clients as dict (topic: type)
+
+        # It may happen that the action client fails to send the action goal.
+        self._error = False
+
+        # log_once support
+        self._logged = False
+        self._logfunc = { 'info': Logger.loginfo, 'warn': Logger.logwarn, 'err': Logger.logerr, 'hint': Logger.loghint }
+
+    def load_goal_msg(self, trajectory_ns, trajectory_param):
+        # derive parameter full name
+        if trajectory_ns:
+            trajectory_param = trajectory_ns + '/' + trajectory_param
+
+        # Load FollowStepSequenceGoal from Parameter Server
+        goal_raw = rospy.get_param(trajectory_param)
+
+        if not isinstance(goal_raw, xmlrpclib.Binary):
+            raise TypeError, "ExecuteStepSequence: ROS parameter '" + trajectory_param + "' is not a binary data."
+        # deserialize
+        goal = FollowStepSequenceGoal()
+        goal.deserialize(goal_raw.data)
+        return goal
+
+    def log_once(self, level, msg):
+        if not self._logged:
+            self._logfunc[level](msg)
+            self._logged = True
+
+    def send_goal(self, goal):
+        self._error = False
+        self._logged = False
+        # Remove odometry information from the goal message
+        del goal.base_motion.points[:]
+        # Send the goal
+        try:
+            self._client.send_goal(self._controller, goal)
+        except Exception as e:
+            Logger.logwarn('ExecuteStepSequence: Failed to send the FollowStepSequence command:\n%s' % str(e))
+            self._error = True
+
+
+    def execute(self, userdata):
+        # While this state is active, check if the action has been finished and evaluate the result.
+
+        # Check if the client failed to send the goal.
+        if self._error:
+            return 'failure'
+    
+        # Check if the action has been finished
+        if not self._client.is_active(self._controller):
+            state = self._client.get_state(self._controller)
+            result = self._client.get_result(self._controller)
+            
+            if state == GoalStatus.SUCCEEDED:
+                # everything is good
+                self.log_once('info', 'ExecuteStepSequence: FollowStepSequence action succesed: ' + repr(result.error_code) + " " + result.error_string)
+                return 'success'
+            elif state in [ GoalStatus.ABORTED, GoalStatus.PREEMPTED ]:
+                # perhaps we stuck in midway due to tolerance error or controller switch.
+                self.log_once('warn', 'ExecuteStepSequence: FollowStepSequence action aborted: ' + repr(result.error_code) + " " + result.error_string)
+                return 'partial_movement'
+            elif state in [ GoalStatus.REJECTED, GoalStatus.RECALLED ]:
+                # Execution has not started, perhaps due to invalid goal.
+                self.log_once('warn', 'ExecuteStepSequence: FollowStepSequence action rejected: ' + repr(result.error_code) + " " + result.error_string)
+                if result.error_code in [ FollowStepSequence.TOLERANCE_VIOLATED, FollowStepSequence.UNABLE_TO_APPEND]:
+                    # Starting pose is invalid
+                    return 'invalid_pose'
+            # Any another kind of error.
+            self.log_once('warn', 'ExecuteStepSequence: FollowStepSequence action failed (' + repr(state) + '): ' + repr(result.error_code) + " " + result.error_string)
+            return 'failure'
+
+
+    def on_exit(self, userdata):
+        # Make sure that the action is not running when leaving this state.
+        # A situation where the action would still be active is for example when the operator manually triggers an outcome.
+        
+        if self._client.is_active(self._controller):
+            self._client.cancel(self._controller)
+            Logger.loginfo('ExecuteStepSequence: Cancel active action goal.')
+
