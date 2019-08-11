d1225958188a62cc65bfa0c4beba0b88ad34012f
cromvell <nverzn@gmail.com>
2019-08-11 20:12:06 +0300

    Destination marker launch parametrization written

--- hmi/sweetie_bot_rviz_interactions/src/destination_marker_main.cpp
+++ hmi/sweetie_bot_rviz_interactions/src/destination_marker_main.cpp
@@ -9,12 +9,28 @@ std::shared_ptr<InteractiveMarkerServer> server;
 int main(int argc, char **argv) {
   ros::init(argc, argv, "destination_marker");
 
+  double scale;
+  std::vector<std::string> gait_type_options;
+  std::vector<int> _n_steps_options;
+  double duration;
+  double nominal_height;
+  std::vector<std::string> ee_names;
+
+	ros::param::get("~scale", scale);
+	ros::param::get("~gait_type_options", gait_type_options);
+	ros::param::get("~n_steps_options", _n_steps_options);
+	ros::param::get("~duration", duration);
+	ros::param::get("~nominal_height", nominal_height);
+	ros::param::get("~ee_names", ee_names);
+
   // marker server
 	server.reset( new InteractiveMarkerServer(ros::this_node::getNamespace(),"",false) );
 	ros::Duration(0.1).sleep();
 
-  DestinationMarker destMarker(server, "destination_marker");
-  destMarker.setEndEffectorTargets();
+  std::vector<unsigned> n_steps_options(_n_steps_options.begin(), _n_steps_options.end());
+
+  DestinationMarker destMarker(server, "destination_marker", scale, gait_type_options, n_steps_options, duration, nominal_height);
+  destMarker.setEndEffectorTargets(ee_names);
 
   ROS_INFO("destination_marker has been started!");
 
