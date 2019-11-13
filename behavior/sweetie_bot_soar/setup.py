6f2ae37a180b512bfbd20f9ff039752592cc9d6b
disrecord <goncharovoi@yandex.ru>
2019-11-13 15:09:32 +0300

    Fix python package name (soar -> sweetie_bot_soar)

--- behavior/sweetie_bot_soar/setup.py
+++ behavior/sweetie_bot_soar/setup.py
@@ -4,7 +4,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['soar'],
+    packages=['sweetie_bot_soar'],
     package_dir={'': 'src'},
 )
 
