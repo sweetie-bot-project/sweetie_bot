564a0dac8d747a63486434cbc3a10519d002f84b
disrecord <goncharovoi@yandex.ru>
2021-03-04 15:14:37 +0300

    ExacuteJointTrajectoryAndSay: Place head at nominal pose before absolute animation.

--- behavior/flexbe/sweetie_bot_soar_flexbe_behaviors/src/sweetie_bot_soar_flexbe_behaviors/executejointtrajectoryandsay_sm.py
+++ behavior/flexbe/sweetie_bot_soar_flexbe_behaviors/src/sweetie_bot_soar_flexbe_behaviors/executejointtrajectoryandsay_sm.py
@@ -10,8 +10,10 @@
 from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
 from flexbe_states.decision_state import DecisionState
 from flexbe_states.wait_state import WaitState
+from sweetie_bot_flexbe_states.check_joint_state import CheckJointState
 from sweetie_bot_flexbe_states.execute_joint_trajectory import ExecuteJointTrajectory
 from sweetie_bot_flexbe_states.execute_joint_trajectory_relative import ExecuteJointTrajectoryRelative
+from sweetie_bot_flexbe_states.set_joint_state import SetJointState
 from sweetie_bot_flexbe_states.text_command_state import TextCommandState
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
@@ -51,7 +53,7 @@ class ExecuteJointTrajectoryAndSaySM(Behavior):
 
 
 	def create(self):
-		# x:716 y:131, x:722 y:347, x:717 y:244
+		# x:1265 y:71, x:1214 y:619, x:1262 y:397
 		_state_machine = OperatableStateMachine(outcomes=['succeed', 'failed', 'invalid_pose'])
 		_state_machine.userdata.none = None
 
@@ -60,83 +62,148 @@ class ExecuteJointTrajectoryAndSaySM(Behavior):
 		
 		# [/MANUAL_CREATE]
 
-		# x:662 y:84
-		_sm_waitandsay_0 = OperatableStateMachine(outcomes=['finished'])
+		# x:542 y:219
+		_sm_say_0 = OperatableStateMachine(outcomes=['finished'])
 
-		with _sm_waitandsay_0:
-			# x:123 y:80
-			OperatableStateMachine.add('WaitTextDelay',
+		with _sm_say_0:
+			# x:107 y:189
+			OperatableStateMachine.add('WaitSay',
 										WaitState(wait_time=max(self.text_delay, 0.0)),
 										transitions={'done': 'SayText'},
 										autonomy={'done': Autonomy.Off})
 
-			# x:342 y:73
+			# x:272 y:200
 			OperatableStateMachine.add('SayText',
 										TextCommandState(type='voice/say', command=self.text, topic='control'),
 										transitions={'done': 'finished'},
 										autonomy={'done': Autonomy.Off})
 
 
-		# x:913 y:78, x:905 y:322, x:912 y:201
-		_sm_animation_1 = OperatableStateMachine(outcomes=['finished', 'failed', 'invalid_pose'], input_keys=['none'])
+		# x:796 y:105, x:778 y:418, x:757 y:263
+		_sm_animation_1 = OperatableStateMachine(outcomes=['finished', 'failed', 'invaid_pose'])
 
 		with _sm_animation_1:
-			# x:99 y:134
+			# x:182 y:115
 			OperatableStateMachine.add('WaitAnimation',
 										WaitState(wait_time=max(-self.text_delay, 0.0)),
-										transitions={'done': 'ChoiceAnimationType'},
+										transitions={'done': 'RelativeAnimation'},
 										autonomy={'done': Autonomy.Off})
 
-			# x:274 y:137
-			OperatableStateMachine.add('ChoiceAnimationType',
-										DecisionState(outcomes=['absolute', 'relative'], conditions=lambda x: 'relative' if self.is_relative else 'absolute'),
-										transitions={'absolute': 'Animation', 'relative': 'RelativeAnimation'},
-										autonomy={'absolute': Autonomy.Off, 'relative': Autonomy.Off},
-										remapping={'input_value': 'none'})
-
-			# x:548 y:58
+			# x:412 y:121
 			OperatableStateMachine.add('RelativeAnimation',
 										ExecuteJointTrajectoryRelative(action_topic='motion/controller/joint_trajectory', trajectory_param=self.joint_trajectory, trajectory_ns='saved_msgs/joint_trajectory', joints_limits=self.get_joint_limits(), joint_states_topic='joint_states'),
-										transitions={'success': 'finished', 'partial_movement': 'invalid_pose', 'invalid_pose': 'invalid_pose', 'failure': 'failed'},
-										autonomy={'success': Autonomy.Off, 'partial_movement': Autonomy.Off, 'invalid_pose': Autonomy.Off, 'failure': Autonomy.Off})
-
-			# x:566 y:233
-			OperatableStateMachine.add('Animation',
-										ExecuteJointTrajectory(action_topic='motion/controller/joint_trajectory', trajectory_param=self.joint_trajectory, trajectory_ns='saved_msgs/joint_trajectory'),
-										transitions={'success': 'finished', 'partial_movement': 'invalid_pose', 'invalid_pose': 'invalid_pose', 'failure': 'failed'},
+										transitions={'success': 'finished', 'partial_movement': 'invaid_pose', 'invalid_pose': 'invaid_pose', 'failure': 'failed'},
 										autonomy={'success': Autonomy.Off, 'partial_movement': Autonomy.Off, 'invalid_pose': Autonomy.Off, 'failure': Autonomy.Off})
 
 
-		# x:321 y:429, x:763 y:412, x:930 y:162, x:931 y:229, x:515 y:419, x:929 y:91
-		_sm_animationandsay_2 = ConcurrencyContainer(outcomes=['finished', 'failed', 'invalid_pose'], input_keys=['none'], conditions=[
-										('finished', [('WaitAndSay', 'finished'), ('Animation', 'finished')]),
-										('failed', [('Animation', 'failed')]),
-										('invalid_pose', [('Animation', 'invalid_pose')])
+		# x:122 y:483, x:449 y:487, x:284 y:481, x:662 y:471, x:700 y:425, x:711 y:488
+		_sm_sayandrelativeanimation_2 = ConcurrencyContainer(outcomes=['finished', 'failed', 'invaid_pose'], conditions=[
+										('finished', [('Say', 'finished'), ('Animation', 'finished')]),
+										('invaid_pose', [('Animation', 'invaid_pose')]),
+										('failed', [('Animation', 'failed')])
 										])
 
-		with _sm_animationandsay_2:
-			# x:474 y:182
-			OperatableStateMachine.add('Animation',
-										_sm_animation_1,
-										transitions={'finished': 'finished', 'failed': 'failed', 'invalid_pose': 'invalid_pose'},
-										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit, 'invalid_pose': Autonomy.Inherit},
-										remapping={'none': 'none'})
-
-			# x:118 y:186
-			OperatableStateMachine.add('WaitAndSay',
-										_sm_waitandsay_0,
+		with _sm_sayandrelativeanimation_2:
+			# x:81 y:160
+			OperatableStateMachine.add('Say',
+										_sm_say_0,
 										transitions={'finished': 'finished'},
 										autonomy={'finished': Autonomy.Inherit})
+
+			# x:292 y:138
+			OperatableStateMachine.add('Animation',
+										_sm_animation_1,
+										transitions={'finished': 'finished', 'failed': 'failed', 'invaid_pose': 'invaid_pose'},
+										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit, 'invaid_pose': Autonomy.Inherit})
+
+
+		# x:572 y:166
+		_sm_say_3 = OperatableStateMachine(outcomes=['finished'])
+
+		with _sm_say_3:
+			# x:123 y:135
+			OperatableStateMachine.add('WaitText',
+										WaitState(wait_time=max(self.text_delay, 0.0)),
+										transitions={'done': 'SayText'},
+										autonomy={'done': Autonomy.Off})
+
+			# x:308 y:141
+			OperatableStateMachine.add('SayText',
+										TextCommandState(type='voice/say', command=self.text, topic='control'),
+										transitions={'done': 'finished'},
+										autonomy={'done': Autonomy.Off})
+
+
+		# x:762 y:86, x:762 y:342, x:753 y:197
+		_sm_animation_4 = OperatableStateMachine(outcomes=['finished', 'failed', 'invaid_pose'])
+
+		with _sm_animation_4:
+			# x:132 y:143
+			OperatableStateMachine.add('WaitAnimation',
+										WaitState(wait_time=max(-self.text_delay, 0.0)),
+										transitions={'done': 'ExecuteJointTrajectory'},
+										autonomy={'done': Autonomy.Off})
+
+			# x:352 y:151
+			OperatableStateMachine.add('ExecuteJointTrajectory',
+										ExecuteJointTrajectory(action_topic='motion/controller/joint_trajectory', trajectory_param=self.joint_trajectory, trajectory_ns='saved_msgs/joint_trajectory'),
+										transitions={'success': 'finished', 'partial_movement': 'invaid_pose', 'invalid_pose': 'invaid_pose', 'failure': 'failed'},
+										autonomy={'success': Autonomy.Off, 'partial_movement': Autonomy.Off, 'invalid_pose': Autonomy.Off, 'failure': Autonomy.Off})
+
+
+		# x:299 y:423, x:587 y:423, x:473 y:434, x:761 y:487, x:861 y:500, x:810 y:460
+		_sm_sayandabsoluteanimation_5 = ConcurrencyContainer(outcomes=['finished', 'failed', 'invalid_pose'], conditions=[
+										('finished', [('Say', 'finished'), ('Animation', 'finished')]),
+										('invalid_pose', [('Animation', 'invaid_pose')]),
+										('failed', [('Animation', 'failed')])
+										])
+
+		with _sm_sayandabsoluteanimation_5:
+			# x:127 y:147
+			OperatableStateMachine.add('Say',
+										_sm_say_3,
+										transitions={'finished': 'finished'},
+										autonomy={'finished': Autonomy.Inherit})
+
+			# x:419 y:152
+			OperatableStateMachine.add('Animation',
+										_sm_animation_4,
+										transitions={'finished': 'finished', 'failed': 'failed', 'invaid_pose': 'invalid_pose'},
+										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit, 'invaid_pose': Autonomy.Inherit})
 
 
 
 		with _state_machine:
-			# x:259 y:129
-			OperatableStateMachine.add('AnimationAndSay',
-										_sm_animationandsay_2,
+			# x:282 y:95
+			OperatableStateMachine.add('ChoiceAnimationType',
+										DecisionState(outcomes=['absolute', 'relative'], conditions=lambda x: 'relative' if self.is_relative else 'absolute'),
+										transitions={'absolute': 'CheckHeadPose', 'relative': 'SayAndRelativeAnimation'},
+										autonomy={'absolute': Autonomy.Off, 'relative': Autonomy.Off},
+										remapping={'input_value': 'none'})
+
+			# x:715 y:180
+			OperatableStateMachine.add('SayAndAbsoluteAnimation',
+										_sm_sayandabsoluteanimation_5,
 										transitions={'finished': 'succeed', 'failed': 'failed', 'invalid_pose': 'invalid_pose'},
-										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit, 'invalid_pose': Autonomy.Inherit},
-										remapping={'none': 'none'})
+										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit, 'invalid_pose': Autonomy.Inherit})
+
+			# x:712 y:39
+			OperatableStateMachine.add('SayAndRelativeAnimation',
+										_sm_sayandrelativeanimation_2,
+										transitions={'finished': 'succeed', 'failed': 'failed', 'invaid_pose': 'invalid_pose'},
+										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit, 'invaid_pose': Autonomy.Inherit})
+
+			# x:576 y:398
+			OperatableStateMachine.add('SetHeadNominal',
+										SetJointState(controller='motion/controller/joint_state_head', pose_param='head_nominal', pose_ns='saved_msgs/joint_state', tolerance=0.03, timeout=10.0, joint_topic="joint_states"),
+										transitions={'done': 'SayAndAbsoluteAnimation', 'failed': 'failed', 'timeout': 'invalid_pose'},
+										autonomy={'done': Autonomy.Off, 'failed': Autonomy.Off, 'timeout': Autonomy.Off})
+
+			# x:351 y:333
+			OperatableStateMachine.add('CheckHeadPose',
+										CheckJointState(outcomes=['head_nominal', 'unknown'], pose_ns='saved_msgs/joint_state', tolerance=0.17, joint_topic="joint_states", timeout=1.0),
+										transitions={'head_nominal': 'SayAndAbsoluteAnimation', 'unknown': 'SetHeadNominal'},
+										autonomy={'head_nominal': Autonomy.Off, 'unknown': Autonomy.Off})
 
 
 		return _state_machine
