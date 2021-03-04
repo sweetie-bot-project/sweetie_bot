c62b874b0462088e19bcada2b68b601846b7f4d2
Cromvell <nverzn@gmail.com>
2021-03-04 22:02:52 +0300

    Changed synthesizer parameters a bit

--- behavior/sweetie_bot_voice/scripts/rhvoice.sh
+++ behavior/sweetie_bot_voice/scripts/rhvoice.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # issue speech sinnthesis
-echo $1 | RHVoice-client -r -0.18 -s Anna+CLB | gst-launch-1.0 fdsrc fd=0 ! decodebin ! autoaudiosink
+echo $1 | RHVoice-client -r -0.2 -v 1.0 -s Anna+CLB | gst-launch-1.0 fdsrc fd=0 ! decodebin ! autoaudiosink
--- behavior/sweetie_bot_voice/scripts/rhvoice_robotized.sh
+++ behavior/sweetie_bot_voice/scripts/rhvoice_robotized.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 export LADSPA_PATH=~/.ladspa/
+# For debian
 export JAVA_HOME=/lib/jvm/adoptopenjdk-8-openj9-amd64/
+# For ubuntu
+#export JAVA_HOME=/lib/jvm/java-8-openjdk-amd64/
 
 # fix broken ladspa support
 #rm -f ~/.cache/gstreamer-1.0/registry.x86_64.bin; 
 #gst-inspect-1.0 ladspa
 # issue speech sinnthesis
-echo $1 | RHVoice-client -r -0.18 -s Anna+CLB | gst-launch-1.0 fdsrc fd=0 ! decodebin ! audioconvert ! audioresample ! ladspa-autotalent-so-autotalent \
+echo $1 | RHVoice-client -r -0.2 -v 1.0 -s Anna+CLB | gst-launch-1.0 fdsrc fd=0 ! decodebin ! audioconvert ! audioresample ! ladspa-autotalent-so-autotalent \
         concert-a="440.48001"           \
         fixed-pitch="0.0"               \
         pull-to-fixed-pitch="0.384"     \
