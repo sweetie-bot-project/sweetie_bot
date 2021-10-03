d5fff422570feb9c8bcc1c5c4c7ff0e0930cb4f0
disrecord <goncharovoi@yandex.ru>
2021-10-03 22:47:38 +0300

    After merge fix: NOp soar input module.

--- behavior/sweetie_bot_soar/src/sweetie_bot_soar/output_modules/nop.py
+++ behavior/sweetie_bot_soar/src/sweetie_bot_soar/output_modules/nop.py
@@ -3,6 +3,6 @@ from . import output_module
 class NOp(output_module.OutputModule):
 
     def __init__(self, config):
-        super(Nop, self).__init__("nop")
+        super(NOp, self).__init__("nop")
 
-output_module.register("nop", Nop)
+output_module.register("nop", NOp)
