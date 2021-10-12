9dfda95243beed0bd587643cfc6d50b424f31004
disrecord <goncharovoi@yandex.ru>
2021-10-12 17:12:56 +0300

    Fixed result status of text commands SOAR output module.

--- behavior/sweetie_bot_soar/src/sweetie_bot_soar/output_modules/text.py
+++ behavior/sweetie_bot_soar/src/sweetie_bot_soar/output_modules/text.py
@@ -19,16 +19,16 @@ class TextCmd(output_module.OutputModule):
         type_id = cmd_id.FindByAttribute("type", 0)
         if not type_id: # or type_id.GetValueType() != "string":
             rospy.logerr("textcmd output module: type attribute is empty")
-            return "error"
+            return "failed"
         command_id = cmd_id.FindByAttribute("command", 0)
         if not command_id: # or command_id.GetValueType() != "string": 
             rospy.logerr("textcmd output module: command attribute is empty")
-            return "error"
+            return "failed"
         # publish message
         msg = TextCommand()
         msg.type = type_id.GetValueAsString()
         msg.command = command_id.GetValueAsString()
         self._cmd_pub.publish(msg)
-        return "completed"
+        return "succeed"
 
 output_module.register("textcmd", TextCmd)
