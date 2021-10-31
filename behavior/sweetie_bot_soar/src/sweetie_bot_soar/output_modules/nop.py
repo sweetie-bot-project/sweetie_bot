c057ec9ff8de9bf93e02747153e7a4514ebad594
disrecord <goncharovoi@yandex.ru>
2021-10-31 12:21:25 +0300

    NOp SOAR output module calls sleep() to trottle SOAR execution. Delay can be controlled from parameteres.

--- behavior/sweetie_bot_soar/src/sweetie_bot_soar/output_modules/nop.py
+++ behavior/sweetie_bot_soar/src/sweetie_bot_soar/output_modules/nop.py
@@ -1,8 +1,17 @@
 from . import output_module
 
+import rospy
+
 class NOp(output_module.OutputModule):
 
     def __init__(self, config):
         super(NOp, self).__init__("nop")
+        self._delay = config.get("delay")
+        if not isinstance(self._delay, (int, float)) or self._delay < 0:
+            raise RuntimeError("NOp output module: 'delay' must be provided and be a positive number.")
+
+    def startHook(self, cmd_id):
+        rospy.sleep(self._delay)
+        return "succeed"
 
 output_module.register("nop", NOp)
