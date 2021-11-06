2b9c8103a331656786725d1649692c0b84ae1ee2
cromvell <nverzn@gmail.com>
2021-11-06 16:52:24 +0300

    Changed locale to English for decimal point unification

--- hmi/sweetie_bot_joint_trajectory_editor/src/main.cpp
+++ hmi/sweetie_bot_joint_trajectory_editor/src/main.cpp
@@ -1,10 +1,13 @@
 #include "trajectoryeditor.h"
 #include <QApplication>
+#include <QLocale>
 
 int main(int argc, char *argv[])
 {
     ros::init(argc, argv, "trajectory_editor");
     ros::NodeHandle node;
+
+    QLocale::setDefault(QLocale::English);
 
     QApplication a(argc, argv);
     TrajectoryEditor *w = new TrajectoryEditor(argc, argv, node);
