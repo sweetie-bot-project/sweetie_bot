c76776694d11258ad0c1840bd122c19ec0ebf0c1
disRecord <goncharovoi@yandex.ru>
2023-05-11 16:06:14 +0300

    Rewrite llm_service_node as class. Add CompleteRaw service support.

--- behavior/sweetie_bot_llm/src/sweetie_bot_llm/main.py
+++ behavior/sweetie_bot_llm/src/sweetie_bot_llm/main.py
@@ -1,94 +1,139 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os, time, json
-import rospy
-from sweetie_bot_text_msgs.srv import Complete, CompleteRequest, CompleteResponse
-from sweetie_bot_text_msgs.srv import CompleteSimple, CompleteSimpleRequest, CompleteSimpleResponse
 import requests
+from copy import copy
+
+import rospy
 from rospy_message_converter import message_converter
 
-urls = {'0': "http://127.0.0.1:5000/api/v1/generate"}
-profiles = {}
+from sweetie_bot_text_msgs.srv import Complete, CompleteRequest, CompleteResponse
+from sweetie_bot_text_msgs.srv import CompleteSimple, CompleteSimpleRequest, CompleteSimpleResponse
+from sweetie_bot_text_msgs.srv import CompleteRaw, CompleteRawRequest, CompleteRawResponse
+from sweetie_bot_text_msgs.msg import TextCommand
 
-def handle_complete(req_message):
-    global urls
-    request = message_converter.convert_ros_message_to_dictionary(req_message)
-    r = None
-    for n in sorted(urls):
-        rospy.loginfo("Send %d bytes to %s" % (len(json.dumps(request)), urls[n]))
+
+class CompleteError(RuntimeError):
+    def __init__(self, msg, details = ''):
+        super(CompleteError, self).__init__(msg)
+        self._details = details
+
+    @property
+    def details(self):
+        return self._details
+
+class CompleteNode:
+
+    def __init__(self):
+        rospy.init_node('complete_server')
+        # get server URLS
+        urls = rospy.get_param("~api_url", {'0': "http://127.0.0.1:5000/api/v1/generate"})
+        self._urls = [ urls[k] for k in sorted(urls) ]
+        # get profiles
+        self._profiles = rospy.get_param("~profile", {})
+        # create services
+        rospy.Service('complete', Complete, self.complete_callback)
+        rospy.Service('input_text', CompleteSimple, self.input_text_callback)
+        rospy.Service('llm_request', CompleteRaw, self.complete_simple_callback)
+        # log
+        rospy.loginfo('urls: %s', self._urls )
+        rospy.loginfo('profiles: %s', [self._profiles.keys()] )
+
+    def request_server(self, request):
+        # display request
+        rospy.logdebug(f'request: \n\n %s \n\n' % request)
+        # send request to servers
+        resp = None
+        for url in self._urls:
+            try:
+                start = time.time()
+                resp = requests.post(url, json=request)
+                duration = time.time() - start
+                if resp.status_code == 200:
+                    break
+                else:
+                    rospy.logwarn("server $s request error %d: %s" % (url, resp.status_code, resp.reason))
+                    continue # next url
+            except requests.ConnectionError as e:
+                rospy.logwarn("connection error: %s" % e.what())
+
+        # check if response is received
+        if resp is None:
+            raise CompleteError('unable to get response: tried all servers.')
+        # decode
         try:
-            start = time.time()
-            r = None
-            rospy.loginfo(request)
-            r = requests.post(urls[n], json=request)
-            duration = time.time() - start
-            if r.status_code == 200:
-                break
-            else:
-                rospy.logerr("%d %s" % (r.status_code, r.reason))
-                continue # next url
-        except requests.ConnectionError as e:
-            rospy.logerr("Connection failed! %s" % e)
-            # nexu url
+            response = resp.json()
+        except ValueError:
+            raise CompleteError('json: can not decode response', resp.content)
+        # display result
+        rospy.logdebug(f'server %s response: \n\n %s \n\n' % (url, response))
+        # check response structure
+        if ( 'results' not in response or len(response['results'])==0 or 'text' not in response['results'][0]):
+            raise CompleteError('wrong response structure', resp.content)
+        # convert to unicode
+        try:
+            # TODO: what is this? decode and then encode?
+            text = response['results'][0]['text'].encode('utf-8').strip()
+            text = text.decode()
+        except UnicodeDecodeError:
+            return CompleteError('unicode decode error', resp.content)
 
-    if r is None:
-        return CompleteResponse(status="All API URLs are down!")
+        return text, duration
 
-    try:
-        response = r.json()
-    except ValueError:
-        rospy.logerr('Cannot decode response (%s)' % (r.content))
-        return CompleteResponse(status='Cannot decode response')
+    def complete_callback(self, req_message):
+        # prepare request
+        request = message_converter.convert_ros_message_to_dictionary(req_message)
+        # send it to servers
+        try:
+            text, duration = self.request_server(request)
+        except CompleteError as e:
+            rospy.logerr('%s: %s' % (e.what(), e.details))
+            return CompleteResponse(status = e.what())
+        # return result
+        return CompleteResponse(status='ok', text = text, duration=duration)
 
-    if ( 'results' not in response or
-      len(response['results'])==0 or
-      'text' not in response['results'][0]):
-        rospy.logerr('Wrong fields in response! (%s)' % (r.content))
-        return CompleteResponse(status="Wrong fields in response!")
+    def complete_simple_callback(self, msg):
+        # get profile
+        profile = self._profiles.get(msg.profile)
+        if profile is None:
+            rospy.logerr('unknown profile: %s' % msg.profile)
+            return CompleteRawResponse(status_error = CompleteRawResponse.UNKNOWN_PROFILE)
+        # construct request 
+        request = copy(profile) # shallow copy: one level is enought
+        request['prompt'] = msg.prompt
+        if len(msg.stop_list) != 0:
+            request['stopping_strings'] = msg.stop_list
+        # send it to server
+        try:
+            text, duration = self.request_server(request)
+        except CompleteError as e:
+            rospy.logerr('%s: %s' % (e.what(), e.details))
+            return CompleteRawResponse(status_error = CompleteRawResponse.SERVER_UNREACHABLE)
+        # return result
+        return CompleteRawResponse(result = text, error_code = CompleteRawResponse.SUCCESS)
 
-    try:
-        text = response['results'][0]['text'].encode('utf-8').strip()
-    except UnicodeDecodeError:
-        rospy.logerr('Cannot encode text! (%s)' % (r.content))
-        return CompleteResponse(status="Cannot encode text!")
-
-    rospy.loginfo('Complete response (%.2fs): "%s"' % (duration, text.decode()))
-    return CompleteResponse(text=text.decode(), status='ok', duration=duration)
-
-def handle_text(msg):
-    global profiles
-    resp = CompleteSimpleResponse()
-    if msg.data.type != 'complete/request':
-        resp.data.type = "complete/error"
-        resp.data.command = 'Wrong command type, try complete/request'
-        return resp
-
-    language = msg.data.options
-    if language in profiles:
-        comp_req = CompleteRequest(**profiles[language])
-        comp_req.prompt = comp_req.prompt % msg.data.command
-        comp_res = handle_complete(comp_req)
-        resp.data.type = "complete/response"
-        resp.data.command = comp_res.text
-        resp.data.options = msg.data.options # assume llm respnse the same language as request
-    else:
-        resp.data.type = "complete/error"
-        resp.data.command = 'No such profile'
-        resp.data.options = msg.data.options
-    return resp
-
-def complete_server():
-    global urls
-    global profiles
-    n = rospy.init_node('complete_server')
-    urls = rospy.get_param("~api_url", urls)
-    profiles = rospy.get_param("~profile", {})
-    s = rospy.Service('complete', Complete, handle_complete)
-    s = rospy.Service('input_text', CompleteSimple, handle_text)
-    rospy.loginfo(urls)
-    rospy.loginfo("Ready to complete.")
-    rospy.spin()
+    def input_text_callback(self, msg):
+        # check input command type
+        if msg.data.type != 'complete/request':
+            return CompleteSimpleResponse(data = TextCommand(type = "complete/error", command = 'wrong command type, expected complete/request'))
+        # get profile
+        profile = self._profiles.get(msg.data.options)
+        if profile is None:
+            rospy.logerr('unknown profile: %s' % msg.data.options)
+            return CompleteSimpleResponse(data = TextCommand(type = "complete/error", command = 'unknown profile'))
+        # produce request
+        request = copy(profile)
+        request['prompt'] = request['prompt'] % msg.data.command
+        # send it to server
+        try:
+            text, duration = self.request_server(request)
+        except CompleteError as e:
+            rospy.logerr('%s: %s' % (e.what(), e.details))
+            return CompleteSimpleResponse(data = TextCommand(type = "complete/error", command = e.what()))
+        # return result
+        return CompleteSimpleResponse(data = TextCommand(type = "complete/response", command = text))
 
 def main():
-    complete_server()
+    n = CompleteNode()
+    rospy.spin()
 
