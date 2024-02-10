4e165ead7efc1a4ef0292e3d8723f8e952cb2b91
mutronics <fsdfur@gmail.com>
2024-02-10 20:12:59 +0300

    small fix in ntp test

--- hardware/sweetie_bot_self_test/src/sweetie_bot_self_test/test_ntc.py
+++ hardware/sweetie_bot_self_test/src/sweetie_bot_self_test/test_ntc.py
@@ -2,14 +2,14 @@ import stat
 import os
 import pytest
 import subprocess as sp
-import socket, struct
 
 cmd = f'timedatectl show'
 child = sp.Popen(cmd, shell=True, stdout=sp.PIPE)
 streamdata = child.communicate()[0]
 ntp_data = streamdata.decode().strip()
 ntp = dict(x.split("=") for x in ntp_data.split("\n"))
-ntp = {k[:8]: v for k, v in ntp.items() if v == 'yes' or v == 'no'}
+ntp = {k[:7]: v for k, v in ntp.items() if v == 'yes' or v == 'no' and k in ['CanNTP', 'NTP', 'NTPSynchronized']}
+
 values = [tuple(i[0]) for i in ntp.values()]
 
 class TestNTP:
